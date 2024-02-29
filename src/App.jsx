d11c4161e7a4a78ac0f64b518cc1701c922b072e
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2024-02-29 19:44:33 -0300

    Componente Footer con descripcion listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './Header'
 import MyCard from './MyCard'
+import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           text = "Raza"
           backgroundColor = "danger"
         />
+        <Footer />
       </div>
     </>
   )
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+  return (
+    <div>
+      <p>¡Gracias por elegir adoptar y darles un hogar a nuestros amigos de cuatro patas!</p>
+    </div>
+  )
+}
+
+export default Footer
